9fa0cc40a6bbb3cba81caba3f84a63658eb4174c
nbogie <neillbogie@googlemail.com>
2023-02-25 15:15:22 +0000

    Initialize dev-eslint-experimental

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { greet } from "./greet";
+import { MyComponent } from "./MyComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+
+      {greet("scholar")}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/greet.test.ts
@@ -0,0 +1,5 @@
+import { greet } from "./greet";
+
+test("Thing", () => {
+  expect(greet("Zaphod")).toEqual("Hello, Zaphod");
+});
