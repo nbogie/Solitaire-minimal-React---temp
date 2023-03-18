9c16f7432d684b1731fbc22c9f2c9a60972fccdc
nbogie <neillbogie@googlemail.com>
2023-03-18 19:57:33 +0000

    Solitaire -> SolitaireC

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
-import Solitaire from "./Solitaire";
+import SolitaireC from "./SolitaireC";
 import "../style.css";
 
 function App() {
     return (
         <div className="App">
-            <Solitaire />
+            <SolitaireC />
         </div>
     );
 }
